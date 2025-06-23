43fe10c6f211a431c057ba923101f845c5696204
dxorud <lmkltk55@gmail.com>
2025-06-23 08:04:18 +0000

    project

--- backend/main.py
+++ backend/main.py
@@ -25,7 +25,3 @@ app.include_router(analysis_router, prefix="/analysis")
 @app.get("/")
 def health_check():
     return {"status": "Emotion FastAPI Server Running!"}
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=True)
